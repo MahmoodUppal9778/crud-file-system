2c6a0091cf7a0675bfe730bffc89d6099fec469f
MahmoodUppal9778 <130857475+MahmoodUppal9778@users.noreply.github.com>
2024-09-28 16:48:11 +0500

    second commit

--- index.js
+++ index.js
@@ -4,4 +4,20 @@ const dirPath = path.join(__dirname, 'crud');
 const filePath = `${dirPath}/simple.txt`;
 console.log(dirPath);
 //fs.writeFileSync("simple.txt", "This is a simple text file")
-fs.writeFileSync(filePath, "This is a simple text file")
+//fs.writeFileSync(filePath, "This is a simple text file");
+/*
+fs.readFile(filePath, 'utf8',(err, item)=>{
+    console.log(item);
+})
+*/
+/*
+fs.appendFile(filePath, " this is the appending text", (err)=>{
+    if(!err) console.log("file is updated")
+});    
+*/
+/*
+fs.rename(filePath,`${dirPath}/simpleRenamed.txt`, (err)=>{
+    console.log("file name is updated");
+});
+*/
+fs.unlinkSync(`${dirPath}/simpleRenamed.txt`);
